81e50f51da852fbe3425f641ae833550bcc57607
ashwani <ashwani.singh@daffodilsw.com>
2022-06-19 22:06:30 +0530

    server

--- server.js
+++ server.js
@@ -1,10 +1,18 @@
-const express = require("express");
-const path = require("path");
-const PORT = process.env.PORT || 8000;
 
-express()
-  .use(express.static(path.join(__dirname, "dist")))
-  .get("*", (req, res) => {
-    res.sendFile("index.html", { root: "dist" });
-  })
-  .listen(PORT, () => console.log(`Listening on ${PORT}`));
+    //Install express server
+    const express = require('express');
+    const path = require('path');
+
+    const app = express();
+
+    // Serve only the static files form the dist directory
+    app.use(express.static(__dirname + '/dist'));
+
+    app.get('/*', function(req,res) {
+        
+    res.sendFile(path.join(__dirname+'/dist/index.html'));
+    });
+
+    // Start the app by listening on the default Heroku port
+    app.listen(process.env.PORT || 8000);
+
